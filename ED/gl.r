2a89afd160135bac6cbab9596811344d6feec971
DragonflyStats <kobriendublin@gmail.com>
2014-12-27 18:14:11 +0000

    Create gl.r

---
+++ ED/gl.r
@@ -0,0 +1,11 @@
+
+
+
+# gl(())
+
+## First control, then treatment:
+x <- gl(2,8, labels = c("Control", "Treat"))
+## 20 alternating 1s and 2s
+gl(2, 1, 20)
+## alternating pairs of 1s and 2s
+gl(2, 2, 20)
